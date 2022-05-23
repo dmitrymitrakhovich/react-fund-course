e7196ed7f49ca108f5b29b18fbb5a1cfc392921e
dmitrymitrakhovich <dmitrymitrakhovich@gmail.com>
2022-05-23 21:01:36 +0400

    add dynamic pagination

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,6 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import PostService from "../components/api/PostService";
 import { useFetching } from "../components/hooks/useFetching";
+import { useObserver } from "../components/hooks/useObserver";
 import { usePosts } from "../components/hooks/usePosts";
 import PostFilter from "../components/PostFilter";
 import PostForm from "../components/PostForm";
@@ -9,6 +10,7 @@ import MyButton from "../components/UI/button/MyButton";
 import Loader from "../components/UI/loader/Loader";
 import MyModal from "../components/UI/modal/MyModal";
 import Pagination from "../components/UI/pagination/Pagination";
+import MySelect from "../components/UI/select/MySelect";
 import { getPagesCount } from "../utils/pages";
 
 function Posts() {
@@ -22,19 +24,24 @@ function Posts() {
   const [totalPages, setTotalPages] = useState(0);
   const [limit, setLimit] = useState(10);
   const [page, setPage] = useState(1);
+  const lastElement = useRef();
 
   const [fetchPosts, isPostsLoading, postError] = useFetching(
     async (limit, page) => {
       const response = await PostService.getAll(limit, page);
-      setPosts(response.data);
+      setPosts([...posts, ...response.data]);
       const totalCount = response.headers["x-total-count"];
       setTotalPages(getPagesCount(totalCount, limit));
     }
   );
 
+  useObserver(lastElement, page < totalPages, isPostsLoading, () =>
+    setPage(page + 1)
+  );
+
   useEffect(() => {
     fetchPosts(limit, page);
-  }, []);
+  }, [page, limit]);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
@@ -47,7 +54,6 @@ function Posts() {
 
   const changePage = (page) => {
     setPage(page);
-    fetchPosts(limit, page);
   };
 
   return (
@@ -58,10 +64,27 @@ function Posts() {
       <MyModal visible={modal} setVisible={setModal}>
         <PostForm create={createPost} />
       </MyModal>
-
       <PostFilter filter={filter} setFilter={setFilter} />
+      <MySelect
+        value={limit}
+        onChange={(value) => setLimit(value)}
+        defaultValue='Page count'
+        options={[
+          { value: 5, name: "5" },
+          { value: 10, name: "10" },
+          { value: 25, name: "25" },
+          { value: -1, name: "All posts" },
+        ]}
+      />
       {postError && <h1>{postError}</h1>}
-      {isPostsLoading ? (
+
+      <PostList
+        posts={sortedAndSearchedPosts}
+        title='Post List 1'
+        deletePost={deletePost}
+      />
+      <div ref={lastElement} style={{ height: 20, background: "red" }}></div>
+      {isPostsLoading && (
         <div
           style={{
             display: "flex",
@@ -71,12 +94,6 @@ function Posts() {
         >
           <Loader />
         </div>
-      ) : (
-        <PostList
-          posts={sortedAndSearchedPosts}
-          title='Post List 1'
-          deletePost={deletePost}
-        />
       )}
       <Pagination
         totalPages={totalPages}
